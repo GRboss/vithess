03ab0ffeca9ba838449a1cf94832a122d93f2366
Stefanos Kapiris <grbosss@gmail.com>
2014-02-22 10:27:20 +0200

    Initial commit

---
+++ application/views/_bottom.php
@@ -0,0 +1,6 @@
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="jquery/jquery-1.11.0.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
